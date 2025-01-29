36915e4f1ba268f3c782c063ebb5d1ab998dd4b2
Firdavs Nazarov <fredrickuzb55@gmail.com>
2025-01-29 16:07:54 +0900

    http module

---
+++ module/http.js
@@ -0,0 +1,17 @@
+const http = require('http')
+
+const server = http.createServer((request, response) =>{
+    // request - so'rov
+    //response - javob
+
+    console.log(request.url);
+
+    response.write('Hello world')
+    response.end()
+    
+})
+
+server.listen(3000, () => {
+    console.log('Server has been started on port: 3000');
+    
+})
