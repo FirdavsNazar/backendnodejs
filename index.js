005e04e749584607e37d362865d1428e017b31bf
Firdavs Nazarov <fredrickuzb55@gmail.com>
2025-02-01 22:15:16 +0900

    First mini project by using Get and Post

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
 
-// const userDate= require(`./user`)
-const carDate= require(`./car`)
+// // const userDate= require(`./user`)
+// const carDate= require(`./car`)
 
-console.log(userDate.user);
-userDate.loguser()
+// console.log(userDate.user);
+// userDate.loguser()
 
-console.log(carDate.car);
-carDate.logercar()
-
-
-
+// console.log(carDate.car);
+// carDate.logercar()
 
 
 
 //for checking nodeman 
 
-// const http = require('http')
+const http = require('http')
 
-// const server = http.createServer((request, response) =>{
-//     // request - so'rov
-//     //response - javob
+const server = http.createServer((req, res) =>{
+    // request - so'rov
+    //response - javob
+    if(req.method === 'GET'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
 
-//     console.log(request.url);
+        res.end(
+            `<h2>Send name </h2>
+            <form method="post" action="/">
+                 <input name="name" required placeholder="Enter your name" />
+                 <button type="submit"> Send name </button>
+            </form>` )
+    } else if (req.method === 'POST') {
+        const body = []
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
 
-//     response.write('Hello world 5')
-//     response.end()
+        req.on('data', data =>{
+            body.push(Buffer.from(data))
+        })
+
+        req.on('end', () => {
+            const message = body.toString().split('=') [1]
+            res.end(`Name successfully added: ${message}`)
+        })
+    }
     
-// })
+})
 
-// server.listen(3000, () => {
-//     console.log('Server has been started on port: 3000');
+server.listen(3000, () => {
+    console.log('Server has been started on port: 3000');
     
-// })
+})
 
